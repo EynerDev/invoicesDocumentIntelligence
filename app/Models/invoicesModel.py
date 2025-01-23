cf76bac05395319be17e2d7ae6f77e88969dd8df
Eyner Acosta <eyneracosta96@gmail.com>
2025-01-23 17:17:43 -0500

    23/01/2025 'Agregar columnas de blob_sas y name_invoices en el modelo de Invoice'

--- app/Models/invoicesModel.py
+++ app/Models/invoicesModel.py
@@ -8,22 +8,32 @@ class InvoicesModel(Base):
     __tablename__ = "invoices"
 
     id = Column(Integer, nullable=False, primary_key=True )
-    type_id = Column(Integer, nullable=False)
-    provider_id = Column(Integer, nullable=False)
+    type_id = Column(Integer, nullable=True)
+    provider_id = Column(Integer, nullable=True)
     path_storage = Column(String(500), nullable=False)
     active = Column(Integer, nullable=True, default=1)
     created_at = Column(DateTime, default=current_timestamp())
-    update_at = Column(DateTime, default=current_timestamp(),
+    updated_at = Column(DateTime, default=current_timestamp(),
                         onupdate=current_timestamp())
-
-    def __init__(self, data):
-        self.id = data.get('id')
-
+    blob_sas = Column(String(500), nullable=False)
+    name_invoice = Column(String(250), nullable=False)
+    
+    
+    def __init__(self, type_id,provider_id, path_storage, blob_sas, name_invoice):
+        
+        self.type_id = type_id
+        self.provider_id = provider_id
+        self.path_storage = path_storage
+        self.active = 1  # Por defecto el valor es 1
+        self.blob_sas =   blob_sas
+        self.name_invoice = name_invoice 
     def __repr__(self):
         return {
             "id": self.id,
             "type_id": self.type_id,
             "provider_id": self.provider_id,
             "path_storage": self.path_storage,
+            "name_invoice": self.name_invoice,
+            "blob_sas": self.blob_sas,
             "active": self.active
         }
